c3514d03cf6ea675cd6cb748972aa850ba2aadb0
LucasSGonza <lucassilva.goncalves@hotmail.com>
2023-10-20 14:08:20 -0300

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Squad Apps on 20/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
